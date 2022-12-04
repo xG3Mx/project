66bd5d95c401e848e98490ecce72a5a93c7686ca
xG3Mx <117913732+xG3Mx@users.noreply.github.com>
2022-12-04 03:37:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,41 +15,44 @@ int data()
 		ifstream in;
 		in.open("/tmp/indata.txt");
 		if (out.is_open())
-    	{
+    		{
     		printf("Создал outdata.txt\n");
     		sleep(1);
-    	}
-    	else {
+    		}
+    		else 
+		{
     		printf("ОШИБКА\n");
     		exit(0);
-    	}
+    		}
 		
 		printf("Открываю outdata.txt\n");
-	    sleep(1);
+	        sleep(1);
 
 
 		pid_t p1 = fork();
-		if (p1 == 0){
+		if (p1 == 0)
+		{
 			if (out.is_open())
-				    	{
+				     {
 				      	 while (getline(in, line))
-				       	 {
+				       	  {
 				            out << line << endl;
 				      	  }
-				      	}
+				     }
 
 			system("less /tmp/outdata.txt");
 			printf("Закрыл outdata.txt\n");
 			out.close();
 			return 0;
 
-	    }
+	    	}
 
 
 		
 		pid_t w_pid1 = waitpid(p1, NULL, 0);
 		if (w_pid1 == -1) {exit(0);}
-		if (w_pid1 > 0){
+		if (w_pid1 > 0)
+		{
 			sleep(1);
 			remove("/tmp/outdata.txt");
 			printf("Удалил outdata.txt\n");
